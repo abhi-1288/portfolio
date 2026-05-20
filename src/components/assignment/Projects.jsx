dad0a420f71b0f0ba958b49b0e1bb7ee0b2b986e
abhi-1288 <abhijeetsinghrana2003@gmail.com>
2026-05-20 15:49:11 +0530

    new website

--- src/components/assignment/Projects.jsx
+++ src/components/assignment/Projects.jsx
@@ -11,8 +11,8 @@ function Projects() {
                 <ContentFormat part="part-1">
                     <div className="grid h-fit text-xl space-y-3 font-Pacifico text-sky-500 p-2">
                         <h1>1. <a className="underline" target="_blank" href="https://mui-site.vercel.app/">Weather App & more</a></h1>
-                        <h1>2. <a className="underline" target="_blank" href="https://app-qr-code.vercel.app/">QR App- Full Stack</a></h1>
-                        <h1>3. <a className="underline" target="_blank" href="https://campus-connect-five-sage.vercel.app//">Campus Connect</a></h1>
+                        <h1>2. <a className="underline" target="_blank" href="https://app-qr-code.vercel.app/">QR App- Full Stack</a> FullStack</h1>
+                        <h1>3. <a className="underline" target="_blank" href="https://campus-connect-five-sage.vercel.app//">Campus Connect</a> FullStack</h1>
                     </div>
                 </ContentFormat>
 
@@ -21,7 +21,7 @@ function Projects() {
 
                         <h1>1. <a className="underline" target="_blank" href="https://class-based-todo.netlify.app/">TO-DO in TYPE-SCRIPT REACT </a> Class-Based COMPONENT</h1>
                         <h1>2. <a className="underline" target="_blank" href="https://resume-creater.vercel.app/">Resume Builder For Beginners </a></h1>
-                        <h1>3. <a className="underline" target="_blank" href="https://todo-vanced.vercel.app/">Advanced Todo "LIKE TRELLO"  </a>Desktop site</h1>
+                        <h1>3. <a className="underline" target="_blank" href="https://flowzen-azure.vercel.app">FlowZen | A Kanban App</a> FullStack</h1>
                     </div>
                 </ContentFormat>
             </div>
